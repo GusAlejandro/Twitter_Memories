744b5ac581e286b7cdbb816b98bd51f1558d089b
Gustavo Cornejo <gcornejo85@gmail.com>
2020-03-07 09:17:54 -0800

    started adding logic for extracting twitter archive from GCP cloud

--- celeryworker/tasks.py
+++ celeryworker/tasks.py
@@ -1,3 +1,6 @@
+import fileinput
+import json
+import os
 from celery import Celery
 from google.cloud import storage
 from configuration.app_config import GCPConfig, CeleryConfig
@@ -11,6 +14,16 @@ def initial_tweet_processing(user_id):
     bucket = storage_client.get_bucket(GCPConfig.GCP_STORAGE_BUCKET)
     resource_uri = GCPConfig.URI_PREFIX + user_id + '.json'
     storage_client.download_blob_to_file(resource_uri, CeleryConfig.TEMPSTORAGE + user_id + '.json')
+
+    # after downloading image it's time to convert it 
+    for line in fileinput.input(os.path.join(CeleryConfig.TEMPSTORAGE, user_id+'.json'), inplace=True):
+        if fileinput.lineno() == 1:
+            print(line.replace('window.YTD.tweet.part0 =', ''), end='')
+        else:
+            print(line, end='')
+
+    fileinput.close()
+    tweetList = json.load(open(os.path.join(CeleryConfig.TEMPSTORAGE, user_id + '.json'), 'r'))
     # grab the file from GCP cloud
     # edit file so we can successfully import as json
     # start iterating through the file so we can extract today and tomorrow off the file
